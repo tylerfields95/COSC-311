b7df4edc548fa2164eea198b2b14abfc7b0bf183
tylerfields95 <tylerfields95@gmail.com>
2016-09-28 08:33:42 -0400

    Add files via upload

---
+++ homework 3/src/reverse.java
@@ -0,0 +1,31 @@
+/**
+ * Tyler Fields
+ *
+ *COSC 311
+ *HW 09/21
+ *Fall 2016
+ */
+public class reverse {
+	public static void main (String [] args)
+	{
+		generator gen = new generator();
+		int list [] = new int[10];
+		int temp = 0;
+		int end = list.length-1;
+		
+		gen.generate();
+		gen.collect(list);
+
+		for (int front = 0; front < end; front++, end--)
+		{
+			temp = list[end];
+			list[end] = list[front];
+			list[front] = temp;
+		}
+		
+		for (int count = 0; count < list.length; count++)
+		{
+			System.out.print(list[count] + " ");
+		}
+	}
+}
